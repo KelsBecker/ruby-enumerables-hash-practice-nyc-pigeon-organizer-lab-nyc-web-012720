6e4def58b6bf650055048ac1ff231e778de9abf9
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-17 15:06:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data) #data is the entire hash of hash of array
   new_hash = {}
   data.each do |key, value| #key is the color key, value is the hash associated with that key
   value.each do |new_key, names| #new key is each color key, names is the array of pigeons names
-  names.each do |name| 
+  names.each do |name| #name is each pigeons name *because names is an array of pigeons I can only use 1 variable instead of 2 when its a hash
     
   if !new_hash[name]
     new_hash[name] = {}
