a1b62ffaeb8aa8da4b1e2394784beefb20d258fd
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-17 14:45:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value| 
-    value.each do 
+  value.each do |new_key, names|
+  names.each do 
   binding.pry
+  end
   end
 end
