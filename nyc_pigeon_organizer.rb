89e33fabcc4530218b355ac940d4b3b87fbe5452
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-17 15:04:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 require 'pry'
 def nyc_pigeon_organizer(data) #data is the entire hash of hash of array
   new_hash = {}
-  data.each do |key, value| #
-  value.each do |new_key, names|
+  data.each do |key, value| #key is the color key, value is the hash associated with that key
+  value.each do |new_key, names| #
   names.each do |name|
     
   if !new_hash[name]
